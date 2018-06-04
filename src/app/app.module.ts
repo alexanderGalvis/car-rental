416f0521ce31950a6e76d9e1d95dd62d5769467e
= <galcorp@gmail.com>
2018-06-04 16:03:36 -0400

    lista de vehiculos y font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from './public/home/home.component';
+import { VehiclesComponent } from './public/vehicles/vehicles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    VehiclesComponent
   ],
   imports: [
     BrowserModule
